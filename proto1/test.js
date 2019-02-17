8a39b000abaec5d249ec18c265bc319a0b470dd0
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2019-02-16 22:48:38 -0500

    Update test.js
    This should fix LED issue

--- proto1/test.js
+++ proto1/test.js
@@ -8,21 +8,21 @@ var Auth = '4fd693da193a43b1baaa489a4b73c7af';
 var Blynk = new Blynk.Blynk(Auth);
 
 
-
 var sensor_1 = new Blynk.VirtualPin(0);//takes fuel amount
 var sensor_2 = new Blynk.VirtualPin(1);//is button
-var sensor_3 = new Blynk.VirtualPin(0);
 
-//A - car drives
 
 sensor_2.on('write', function(param){
 
-	if (sensor_1 == '1'){
+	if (sensor_1 > 0 ){
 	  led.writeSync(1);
+	  console.log('Hi');
 	} else{
 	  led.writeSync(0);
 	}
 
-	console.log('V0', param[0]);
+	console.log('V0', param[0]):
 
 });
+
+
