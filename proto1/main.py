49c80963458fdeeceafd7e7c4d7cf2a2d624bc61
Jin Sing Sia <jinsing.sia@gmail.com>
2019-02-17 09:38:27 -0500

    Prototype 1 code works

--- proto1/main.py
+++ proto1/main.py
@@ -37,84 +37,94 @@ seq_cw = [
 # Counterclockwise solenoid energization sequence
 seq_ccw = seq_cw[::-1]
 
+GPIO.add_event_detect(btn_A, GPIO.RISING, \
+	bouncetime=200)
+GPIO.add_event_detect(btn_B, GPIO.RISING, \
+	bouncetime=200)
+
 # Motor functions
+def stop_motors(pin=-1):
+	if pin == btn_A:
+		GPIO.output(motors[0], 0)
+		print("motor A stop")
+	elif pin == btn_B:
+		GPIO.output(motors[1], 0)
+		print("motor B stop")
+	else:
+		GPIO.output(motors[0], 0)
+		GPIO.output(motors[1], 0)
+
 def move_A(duration, dir=True, dt=0.002):
 	# Duration in seconds
 	# dir = True => CW
 	# dt is motor step time
 	start = time()
-	if dir:
+	stop = False
+	if dir == 1:
 		seq = seq_cw
-	else:
+	elif dir == -1:
 		seq = seq_ccw
-	while time() < start + duration:
+	else:
+		return
+	while time() < start + duration and stop == False:
 		for step in seq:
-				GPIO.output(motors[0], step)
-				sleep(dt)
+			GPIO.output(motors[0], step)
+			sleep(dt)
+			if GPIO.event_detected(btn_A):
+				print("Motor A stop")
+				stop = True
+				break
+				
 	# Motors off
-	GPIO.output(motors[0], 0)
+	stop_motors()
 
 def move_B(duration, dir=True, dt=0.002):
 	# Duration in seconds
+	# Dir = True => CW
 	# dt is motor step time
 	start = time()
-	if dir:
+	stop = False
+	if dir == 1:
 		seq = seq_cw
-	else:
+	elif dir == -1:
 		seq = seq_ccw
-	while time() < start + duration:
+	else:
+		return
+	while time() < start + duration and stop == False:
 		for step in seq:
-				GPIO.output(motors[1], step)
-				sleep(dt)
-	# Motors off
-	GPIO.output(motors[1], 0)
-	
-#Shawn's stuff	
-def click_button(button_type):
-	# waiting for button press
-	while True:
-		try:
-		input_state = GPIO.input(btn_B)
-			if input_state = True:
-				print('Buttton Pressed to Stop Motors')
-				# Stop all motors
-				GPIO.output(motors[0], 0)
-				GPIO.output(motors[1], 0)
-				print("Motors off.")
+			GPIO.output(motors[1], step)
+			sleep(dt)
+			if GPIO.event_detected(btn_B):
+				print("Motor B stop")
+				stop = True
 				break
-
-	except KeyboardInterrupt:
-		print("Keyboard interrupt.")
-		
-		
+	# Motors off
+	stop_motors()
 
 try:
-	move_A(1, True)
-	move_B(1, True)
+	print("1: Motor A up")
+	timeUp = 5
+	move_A(timeUp)
 
-	# Wait for buttonpress
-	while True:
-		input_state = GPIO.input(btn_A)
-		n = 0
-		if input_state == True:
-			print('Button Pressed')
-			n += 1
-			sleep(0.2)
-		if n >= 10:
-			break
+	print("2: Motor B forward")
+	move_B(20)
 
-	move_A(1, False)
-	move_B(1, False)
+	print("3: Wait 5s")
+	sleep(5)
+
+	print("4: Motor B backward")
+	move_B(20, -1)
+
+	print("5: Motor A down")
+	move_A(20, -1)
+
 except KeyboardInterrupt:
 	print("Keyboard interrupt.")
+
 finally:
 	# Stop all motors
-	GPIO.output(motors[0], 0)
-	GPIO.output(motors[1], 0)
+	stop_motors()
 	print("Motors off.")
 
 	# Reset all GPIO pins
 	GPIO.cleanup()
-	
-	
-	
